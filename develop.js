6a4b791d7c27cf195bb760a46ad81d4b571419cb
Baris Bilgin <brokoliweb@gmail.com>
2020-12-05 09:20:11 +0300

    fix style issue add multiplication and addition

--- develop.js
+++ develop.js
@@ -1,16 +1,20 @@
-let str = "51.5+6";
+let str = "55+5-2";
 
-let array = str.split("+");
+let array = str.split(/\W+/);
+
 
 
 console.log(array);
 
-let total = 0;
+let total = array[0] ;
 
-for ( let i =0; i < array.length; i++) {
+for ( let i =1; i < array.length; i++) {
+
     console.log(array[i]);
-    total+= parseFloat(array[i]);
+    total/= parseFloat(array[i]);
     
 }
 
 console.log(total);
+
+console.log(10/5);
--- main.js
+++ main.js
@@ -4,12 +4,16 @@ const total = document.querySelector(".total");
 const numbers = document.querySelectorAll(".numbers");
 
 const equal = document.getElementById("equal");
+const clearAll = document.getElementById("clear-all");
+const clearLast = document.getElementById("clear-last");
+const backspace = document.getElementById("backspace");
 
 numbers.forEach((element) => {
   element.addEventListener("click", numberClicked);
 });
 
 equal.addEventListener("click", compute);
+clearAll.addEventListener("click", deleteAll);
 
 function numberClicked(e) {
   console.log(e);
@@ -19,47 +23,74 @@ function numberClicked(e) {
 }
 
 function compute(e) {
-    let str = totalOngoing.textContent;
-    
-    if (str.includes("+")) {
-        addition();
-    } else if (str.includes("-")) {
-        subtraction();
-    }
-  
-  
+  let str = totalOngoing.textContent;
+
+  if (str.includes("+")) {
+    addition();
+  } else if (str.includes("-")) {
+    subtraction();
+  } else if (str.includes("*")) {
+    multiplication();
+  } else if (str.includes("/")) {
+    division();
+  }
 }
 
 function addition() {
-    let str = totalOngoing.textContent;
-    let array = str.split("+");
-  console.log(array);
+  let str = totalOngoing.textContent;
+  let array = str.split(/\W+/);
+  console.table(array);
   let sum = 0;
 
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length-1; i++) {
     console.log(array[i]);
     sum += parseFloat(array[i]);
-    
+
     total.textContent = sum;
-    
   }
-  totalOngoing.textContent= "";
+  totalOngoing.textContent = total.textContent;
 }
 
 function subtraction() {
-    let str = totalOngoing.textContent;
-    let array = str.split("-");
+  let str = totalOngoing.textContent;
+  let array = str.split(/\W+/);
   console.log(array);
   let sum = 0;
 
-  for (let i = 0; i < array.length; i++) {
-    
-    sum -= parseFloat(array[i]) ;
-    console.log(array[0]);
-    console.log(array[1]);
-    console.log(array[2]);
-    total.textContent = sum + (array[0]*2);
-    
+  for (let i = 0; i < array.length-1; i++) {
+    sum -= parseFloat(array[i]);
+    total.textContent = sum + array[0] * 2;
   }
-  totalOngoing.textContent= "";
+  totalOngoing.textContent = total.textContent;
+}
+
+function multiplication() {
+  let str = totalOngoing.textContent;
+  let array = str.split(/\W+/);
+  console.log(array);
+  let sum = array[0];
+
+  for (let i = 1; i < array.length-1; i++) {
+    sum *= parseFloat(array[i]);
+    total.textContent = sum;
+  }
+  totalOngoing.textContent = total.textContent;
+}
+
+function division() {
+  let str = totalOngoing.textContent;
+  let array = str.split(/\W+/);
+  console.log(array);
+  let sum = array[0];
+
+  for (let i = 1; i < array.length-1; i++) {
+    sum /= parseFloat(array[i]);
+    total.textContent = sum;
+  }
+  totalOngoing.textContent = total.textContent;
+}
+
+function deleteAll() {
+  totalOngoing.textContent = "";
+  total.textContent = 0;
 }
