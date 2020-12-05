61da8c31360bec6c724ddaa6709d701cc28c0965
Baris Bilgin <brokoliweb@gmail.com>
2020-12-05 16:04:47 +0300

    fix style.css not loading add .

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
+
+
 const totalOngoing = document.querySelector(".total-ongoing");
 const total = document.querySelector(".total");
 
-const numbers = document.querySelectorAll(".numbers");
+const number = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
 
 const equal = document.getElementById("equal");
 const clearAll = document.getElementById("clear-all");
 const clearLast = document.getElementById("clear-last");
 const backspace = document.getElementById("backspace");
 
-numbers.forEach((element) => {
+
+number.forEach((element) => {
+  element.addEventListener("click", numberClicked);
+});
+operator.forEach((element) => {
   element.addEventListener("click", numberClicked);
 });
 
 equal.addEventListener("click", compute);
 clearAll.addEventListener("click", deleteAll);
 
+
 function numberClicked(e) {
   console.log(e);
   num = e.target.textContent;
-
   totalOngoing.textContent += num;
+  
+  
 }
 
 function compute(e) {
@@ -38,14 +47,12 @@ function compute(e) {
 
 function addition() {
   let str = totalOngoing.textContent;
-  let array = str.split(/\W+/);
-  console.table(array);
+  let array = str.split(/[\*\+\x2D\/]/);
   let sum = 0;
 
-  for (let i = 0; i < array.length-1; i++) {
-    console.log(array[i]);
+  for (let i = 0; i < array.length; i++) {
     sum += parseFloat(array[i]);
-
+    sum = Math.round(sum*1000) /1000;
     total.textContent = sum;
   }
   totalOngoing.textContent = total.textContent;
@@ -53,12 +60,12 @@ function addition() {
 
 function subtraction() {
   let str = totalOngoing.textContent;
-  let array = str.split(/\W+/);
-  console.log(array);
+  let array = str.split(/[\*\+\x2D\/]/);
   let sum = 0;
 
-  for (let i = 0; i < array.length-1; i++) {
+  for (let i = 0; i < array.length; i++) {
     sum -= parseFloat(array[i]);
+    sum = Math.round(sum*1000) /1000;
     total.textContent = sum + array[0] * 2;
   }
   totalOngoing.textContent = total.textContent;
@@ -66,31 +73,32 @@ function subtraction() {
 
 function multiplication() {
   let str = totalOngoing.textContent;
-  let array = str.split(/\W+/);
-  console.log(array);
+  let array = str.split(/[\*\+\x2D\/]/);
   let sum = array[0];
 
-  for (let i = 1; i < array.length-1; i++) {
+  for (let i = 1; i < array.length; i++) {
     sum *= parseFloat(array[i]);
+    sum = Math.round(sum*1000) /1000;
     total.textContent = sum;
   }
-  totalOngoing.textContent = total.textContent;
+  totalOngoing.textContent = total.textContent
 }
 
 function division() {
   let str = totalOngoing.textContent;
-  let array = str.split(/\W+/);
-  console.log(array);
+  let array = str.split(/[\*\+\x2D\/]/);
   let sum = array[0];
 
-  for (let i = 1; i < array.length-1; i++) {
+  for (let i = 1; i < array.length; i++) {
     sum /= parseFloat(array[i]);
+    sum = Math.round(sum*1000) /1000;
     total.textContent = sum;
   }
-  totalOngoing.textContent = total.textContent;
+  totalOngoing.textContent = total.textContent
 }
 
 function deleteAll() {
   totalOngoing.textContent = "";
   total.textContent = 0;
+  array = [];
 }
